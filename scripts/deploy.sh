07c5993aa81bfe1fc76df87fe20c2d908d5bdbbb
rahulguthi033-tech <rahulguthi033@gmail.com>
2026-04-02 13:31:42 +0530

    Replace Terraform deployment with Nginx setup
    Removed Terraform deployment steps and added Nginx installation and setup.

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,30 +1,6 @@
 #!/bin/bash
-
-echo "===== Terraform Init ====="
-cd ../terraform
-terraform init
-
-echo "===== Terraform Apply ====="
-terraform apply -auto-approve
-
-echo "===== Get PRIVATE IPs from ASG ====="
-aws ec2 describe-instances \
---filters "Name=tag:Name,Values=web-server" "Name=instance-state-name,Values=running" \
---query 'Reservations[*].Instances[*].PrivateIpAddress' \
---output text > ../ansible/inventory
-
-echo "===== Update Inventory ====="
-cd ../ansible
-
-sed -i '1i [web]' inventory
-
-echo "ansible_user=ubuntu" >> inventory
-echo "ansible_ssh_private_key_file=~/your-key.pem" >> inventory
-
-# Optional: Bastion (recommended)
-echo "ansible_ssh_common_args='-o ProxyCommand=\"ssh -i ~/your-key.pem ubuntu@BASTION_PUBLIC_IP -W %h:%p\"'" >> inventory
-
-echo "===== Run Ansible Playbook ====="
-ansible-playbook -i inventory playbook.yml
-
-echo "===== Deployment Completed ====="
+apt update -y
+apt install nginx -y
+systemctl start nginx
+systemctl enable nginx
+echo "Hello from $(hostname)" > /var/www/html/index.html
